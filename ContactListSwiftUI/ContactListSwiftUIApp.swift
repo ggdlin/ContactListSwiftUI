f59f0e77514ba9d846647dec3604a6d4bc280f87
Sergey Ivanov <ggdlin@gmail.com>
2020-12-24 22:01:56 +0300

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Ivanov Sergey on 24.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
